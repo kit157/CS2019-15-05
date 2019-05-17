8fc8bcc75316e02de33c14203f94c37bde87acf0
kit157 <kit157@mail.ru>
2019-05-17 21:35:02 +0300

    lesson2+

---
+++ src/Slesarchyk/Lesson2/src/slesarchyk/lesson2/Lesson2.java
@@ -0,0 +1,12 @@
+package slesarchyk;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+        System.out.println("Я ");
+        System.out.println("иначинаю ");
+        System.out.println("изучать ");
+        System.out.println("Java ");
+        System.out.println("!");
+    }
+}
